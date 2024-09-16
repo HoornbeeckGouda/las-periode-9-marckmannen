16c2cdec803bda9d11f4dd4f98f89497fddcf0aa
Epixness <superhenk4@gmail.com>
2024-09-16 10:16:17 +0200

    Edit user login

--- las/database/migrations/2024_09_03_081432_create_users_table.php
+++ las/database/migrations/2024_09_03_081432_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->foreignId('role_id')->constrained()->on('roles')->onDelete('cascade')->nullable()->default('student');
+            $table->foreignId('student_id')->constrained()->on('students')->onDelete('cascade')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
